510239c3e7e692aabc32eb15d1f8f99196cdb42e
jbhdeconinck <jbhdeconinck@gmail.com>
2015-10-26 17:51:41 +0000

    cat picture

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World"
+  "<div>
+  <img src='http://bit.ly/1eze8aE' style ='border: 3px dashed red'>
+  </div>"
+end
+
+get '/secret' do
+  "Life is beautiful!"
+end
+
+get '/secret/favouritecar' do
+  "Mclaren mp3"
 end
